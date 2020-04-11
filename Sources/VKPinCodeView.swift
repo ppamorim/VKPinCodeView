928af1b9665517468f7eac6f0790716450ff384f
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2020-04-11 16:32:23 +0100

    Replace small dot with big one

--- Sources/VKPinCodeView.swift
+++ Sources/VKPinCodeView.swift
@@ -233,8 +233,7 @@ public final class VKPinCodeView: UIView {
 
         let charIndex: String.Index = text.index(text.startIndex, offsetBy: index)
         let char: String = String(text[charIndex])
-        activeLabel.text = isSecureTextEntry ? "\u{11044}" : char
-//        activeLabel.text = isSecureTextEntry ? "\u{2022}" : char
+        activeLabel.text = isSecureTextEntry ? "\u{2022}" : char
         _code += char
 
     }
